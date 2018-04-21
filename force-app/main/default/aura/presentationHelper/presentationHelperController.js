b5e35b62349d276ea7d5559d8e5611cc86beb6c3
Zayne Turner <16801980+zaynelt@users.noreply.github.com>
2018-04-20 20:22:53 -0700

    adding null var tolerance

--- force-app/main/default/aura/presentationHelper/presentationHelperController.js
+++ force-app/main/default/aura/presentationHelper/presentationHelperController.js
@@ -3,16 +3,20 @@
         var slides = component.find("slides");
         var player = component.find("songHelper");
         var song = component.get("v.walkOnSong");
-        player.start(song);
         slides.slidesKO(slides);
-        window.setTimeout(
-            $A.getCallback(function() {
-                player.stop();
-            }), 16880
-        );
+        if(song){
+            player.start(song);
+            window.setTimeout(
+                $A.getCallback(function() {
+                    player.stop();
+                    //duration may need to change, based on connectivity
+                }), 16880
+            );
+        }
+          
     },
     setSong : function(component, event, helper){
         var select = event.getParam("song");
-        component.set("v.walkOnSong", select);
+        if(select) component.set("v.walkOnSong", select);
     }
 })
